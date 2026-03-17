e7ddfcf29f54300e9c9c7581f618b1de0b8b3388
anandgandharwa-create <anandgandharwa@gmail.com>
2026-03-17 17:59:57 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "zqf-v48";  // <-- हर update पर सिर्फ यही बदलना
+const CACHE_NAME = "zqf-v49";  // <-- हर update पर सिर्फ यही बदलना
 
 const urlsToCache = [
   "/zero-quantum-frequency/",
